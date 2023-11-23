29dcc107230343d5214a95146a02f2fa231f0c7e
andrewMasiero <50714278+andrewMasiero@users.noreply.github.com>
2023-11-23 10:31:30 -0600

    added config.js file

---
+++ config.js
@@ -0,0 +1,14 @@
+const accounts = [
+  "Fidelity Checking",
+  "eFund",
+  "Mia",
+  "Kraken",
+  "Roth IRA",
+  "401k",
+  "Accumulation",
+  "NinjaTrader",
+  "1st Mid Checking",
+  "Capital One Checking",
+];
+
+export { accounts };
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+import { accounts } from "./config.js";
+console.log(accounts);
+
 const main = document.querySelector("main");
-const accounts = ["Checking", "eFund", "Mia"];
 
 for (let account of accounts) {
   main.insertAdjacentHTML("beforeend", `<p>${account}</p>`);
