cd3315977f6a1bc50f13c8ccf05646f67bc6b197
andrewMasiero <50714278+andrewMasiero@users.noreply.github.com>
2023-11-25 14:19:41 -0600

    add account via "Enter" and text

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ import { accounts } from "./config.js";
 console.log(accounts);
 
 const main = document.querySelector("main");
+const accountInput = document.querySelector("#account-name");
+const accountButton = document.querySelector("#account-button");
+const accountForm = document.querySelector("#account-form");
+const testParagraph = document.querySelector("#test");
+
+accountForm.addEventListener("submit", (event) => event.preventDefault());
 
 for (let account of accounts) {
   main.insertAdjacentHTML("beforeend", `<p>${account}</p>`);
 }
+
+accountButton.addEventListener("click", (event) => {
+  if (accountInput.value) {
+    main.insertAdjacentHTML("beforeend", `<p>${accountInput.value}</p>`);
+  } else {
+    console.log("field empty");
+  }
+});
+
+onkeydown = (event) => {
+  if (event.key === "Enter") {
+    if (accountInput === document.activeElement) {
+      accountButton.click();
+    }
+  }
+};
